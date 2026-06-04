a0add4bd8b10a27a6608fc874e381feafdf64f1a
Himanshu pandey <pandeyhimanshu4111@gmail.com>
2026-06-04 17:25:07 +0530

    add variable example in basics folder

---
+++ Basics/Variable.c
@@ -0,0 +1,16 @@
+       
+       /* VARIABLE = a variable is a named storage location which store some data*/
+
+#include <stdio.h>
+
+int main() {
+    int age = 18 ;
+    int price = 100 ;
+    int number = 1;
+
+    printf("age =%d\n",age);
+    printf("price =%d\n",price);
+    printf("number =%d\n",number);
+
+    return 0;
+}

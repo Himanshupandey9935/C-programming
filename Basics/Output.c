9af25425d3a756c6c6433fdbdf9fdf2883849e08
Himanshu pandey <pandeyhimanshu4111@gmail.com>
2026-06-06 16:45:07 +0530

    Add output examples using printf() and format specifiers in C

---
+++ Basics/Output.c
@@ -0,0 +1,26 @@
+
+           // THIS CODE EXPLAINS HOW THE COMPUTER DISPLAYS OUTPUT TO THE USER
+
+#include <stdio.h>
+
+int main() {
+
+// For INTEGER output -
+int age = 22;
+printf("Age = %d\n", age);
+
+// For CHARACTER output -
+char star = '*';
+printf("Star = %c\n", star);
+
+// For FLOAT output -
+float number = 2.2;
+printf("Number = %f\n", number);
+
+// For STRING output -
+char name[] = "Himanshu";
+printf("Name = %s\n", name);
+
+return 0;
+
+}

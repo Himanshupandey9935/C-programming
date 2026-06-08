531c2c027fe0ca9906d98b9519c8195f0c555ec0
Himanshu pandey <pandeyhimanshu4111@gmail.com>
2026-06-08 15:16:09 +0530

    Add basic sum programm

---
+++ 2_Input_Output/Sum.c
@@ -0,0 +1,20 @@
+
+ //                               SUM OF TWO NUMBERS 
+
+ #include <stdio.h>
+
+ int main(){
+    
+    int a;
+    printf("Enter the first number ");
+    scanf("%d", &a);
+
+    int b;
+    printf("Enter the second number ");
+    scanf("%d", &b);
+
+    printf("the sum is = %d ", a + b );
+
+    return 0;
+
+ }

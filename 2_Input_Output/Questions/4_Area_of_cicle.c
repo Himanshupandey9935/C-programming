dddbfa433b93cf1bf0414e61112af5cd8410aab5
Himanshu pandey <pandeyhimanshu4111@gmail.com>
2026-07-26 16:01:40 +0530

    add Area of circle program

---
+++ 2_Input_Output/Questions/4_Area_of_cicle.c
@@ -0,0 +1,17 @@
+// finding area of circle 
+
+#include <stdio.h>
+
+int main() {
+
+    
+    float radius;
+    printf("enter the radius ");
+    scanf("%f", &radius);
+
+   
+    printf("area of circle = %f", radius * radius * 3.14);
+
+    return 0;
+
+}

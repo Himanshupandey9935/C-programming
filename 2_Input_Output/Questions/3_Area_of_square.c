d37e931a489cfc37f48be2e5764117c2db7162e3
Himanshu pandey <pandeyhimanshu4111@gmail.com>
2026-07-26 15:34:07 +0530

    add Area of square program

---
+++ 2_Input_Output/Questions/3_Area_of_square.c
@@ -0,0 +1,16 @@
+// finding area of square 
+
+#include <stdio.h>
+
+int main() {
+    
+    int side;
+    printf("enter the first side ");
+    scanf("%d", &side);
+
+   
+    printf("area of square = %d", side * side);
+
+    return 0;
+
+}
